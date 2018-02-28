a080d86c1ff9f78ac9b858157383b1619111fd15
Eric Griffis <dedbox@gmail.com>
2018-02-28 10:13:09 -0800

    add parser-tools and scribble to deps

--- info.rkt
+++ info.rkt
@@ -2,4 +2,13 @@
 
 (define collection "texmath")
 
-(define deps '("base"))
+(define deps
+  '("base"
+    "parser-tools-lib"
+    "scribble-lib"))
+
+(define build-deps
+  '("at-exp-lib"
+    "scribble-lib"))
+
+(define scribblings '(("scribblings/main.scrbl" () (library) "texmath")))
