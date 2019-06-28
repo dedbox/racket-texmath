9c775542b5473ed6aeedc7c45ecc6726fbd483fc
Eric Griffis <dedbox@gmail.com>
2019-06-27 18:00:08 -0700

    update project dependencies

--- info.rkt
+++ info.rkt
@@ -2,15 +2,12 @@
 
 (define collection "texmath")
 
-(define deps
-  '("base"
-    "functional-lib"
-    "megaparsack-lib"
-    "scribble-lib"))
+(define deps '("base"
+               "functional-lib"
+               "megaparsack-lib"
+               "scribble-lib"))
 
-(define build-deps
-  '("at-exp-lib"
-    "scribble-lib"
-    "rackunit-lib"))
+(define build-deps '("racket-doc"
+                     "scribble-doc"))
 
 (define scribblings '(("scribblings/main.scrbl" () (library) "texmath")))
